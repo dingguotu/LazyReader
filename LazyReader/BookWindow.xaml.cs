722a4498b96f6942c489d18b87d82d631707d339
涂定国 <1335162473@qq.com>
2022-01-18 10:20:39 +0800

    fix merge bug

--- LazyReader/BookWindow.xaml.cs
+++ LazyReader/BookWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
